772319e7228f184904e195156ebaaad15b99db2b
Michael Gecht <gecht.m@gmail.com>
2015-12-02 16:45:30 +0200

    Changed all old domain representations to the new domain.

--- config/settings/production.py
+++ config/settings/production.py
@@ -61,7 +61,7 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['geomat.idiotism.us'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['geomat.uni-frankfurt.de'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ("gunicorn", )

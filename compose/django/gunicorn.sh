96b4067eac1877d93ac3b996d6d3b5c23152d3f6
Michael Gecht <mgecht@stud.uni-frankfurt.de>
2016-03-11 13:50:06 +0100

    Prepared configuration for multi app support

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python /app/manage.py collectstatic --noinput
 python /app/manage.py compilemessages
-/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
+/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5001 --chdir=/app

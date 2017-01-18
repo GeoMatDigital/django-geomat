6ec8d2f6dc3a060b0cad99aaafce08e575792b8f
Michael Gecht <gecht.m@gmail.com>
2017-01-18 16:02:29 +0100

    Removed Amazon S3, as we do not want to use it due to high cost

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,6 @@
 Production Configurations
 
 - Use djangosecure
-- Use Amazon's S3 for storing static files and uploaded media
 - Use mailgun to send emails
 
 - Use sentry for error logging
@@ -13,7 +12,6 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 
-from boto.s3.connection import OrdinaryCallingFormat
 from django.utils import six
 
 from .common import *  # noqa
@@ -72,33 +70,12 @@ INSTALLED_APPS += ("gunicorn", )
 # ------------------------------------------------------------------------------
 # Uploaded Media Files
 # ------------------------
-# See: http://django-storages.readthedocs.org/en/latest/index.html
-INSTALLED_APPS += (
-    'storages',
-)
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
-AWS_AUTO_CREATE_BUCKET = True
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
-
-# AWS cache settings, don't change unless you know what you're doing:
-AWS_EXPIRY = 60 * 60 * 24 * 7
-
-# TODO See: https://github.com/jschneier/django-storages/issues/47
-# Revert the following and use str after the above-mentioned bug is fixed in
-# either django-storage-redux or boto
-AWS_HEADERS = {
-    'Cache-Control': six.b('max-age=%d, s-maxage=%d, must-revalidate' % (
-        AWS_EXPIRY, AWS_EXPIRY))
-}
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
-MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+# TODO: We will change this domain, as soon as the university configures our new one!
+MEDIA_URL = 'https://cdn.geomat.idiotism.us/'
 
 # Static Assets
 # ------------------------

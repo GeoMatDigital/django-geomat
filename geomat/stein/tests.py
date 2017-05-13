a1ca77a0f1a7d55436905f558eddaa409b100c5e
chgad <chgad.games@web.de>
2017-05-13 22:33:20 +0200

    cleaned stein/models.py up and integrated the first test

--- geomat/stein/tests.py
+++ geomat/stein/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import MineralType,Classification, CrystalSystem, Handpiece, Photograph
+
+class MineralTypeTestCase(TestCase):
+    """ This class defines the test suite for the mieraltype model."""
+
+    def setUp(self):
+        self.mineral_type_name = "New MineralType"
+        self.mineral_type = MineralType(name=self.mineral_type_name)
+
+    def test_mineral_catigories_not_empty(self):
+        """
+        1) Test wether mineral Categories choices is empty.
+        """
+        categories = self.mineral_type.MINERAL_CATEGORIES
+
+        self.assertEqual(True, categories)
+
 
 # Create your tests here.

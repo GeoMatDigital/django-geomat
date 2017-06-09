9d947247da021fc23d10d72a98265208d775ea77
Chgad <chgad.games@web.de>
2017-06-09 13:59:50 +0200

    Add routing to geomat app

---
+++ geomat/stein/apiurls.py
@@ -0,0 +1,23 @@
+"""URL definitions for REST framework"""
+from django.conf.urls import url
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from geomat.stein.views import crystalsystem_detail, handpiece_detail, photograph_detail, \
+                               mineraltype_detail
+
+app_name = "api"
+urlpatterns = [
+    # Detail Views for Crystalsystems, handpieces, Photographs and Mineraltypes
+    url(
+        r'^crystalsystem/(?P<pk>[0-9]+)/',
+        crystalsystem_detail,
+        name="crystalsystem"),
+    url(r'^handpiece/(?P<pk>[0-9]+)/', handpiece_detail, name="handpiece"),
+    url(r'^photograph/(?P<pk>[0-9]+)/', photograph_detail, name="photograph"),
+    url(
+        r'^mineraltype/(?P<pk>[0-9]+)/',
+        mineraltype_detail,
+        name="mineraltype"),
+]
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])

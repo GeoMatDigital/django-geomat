88d6b595934bd1abf9b1b83257ee60cfb4816cdc
chgad <chgad.games@gmail.com>
2017-10-26 14:36:51 +0200

    Just visual code formatting

--- geomat/stein/migrations/0035_auto_20171025_1114.py
+++ geomat/stein/migrations/0035_auto_20171025_1114.py
@@ -9,23 +9,23 @@ def eliminate_hyphen_in_variety(apps, schema_editor):
     """This operation elminates hypen-corrupted fields"""
     mineral_type = apps.get_model("stein", "MineralType")
     for m in mineral_type.objects.filter(variety__contains="-").all():
-        
-	m.variety = ""
-	m.save()
+        m.variety = ""
+        m.save()
+
 
 def revert(apps, schema_editor):
     """We do not want to revert this process, so we provide an empty revert function """
     pass
 
-class Migration(migrations.Migration):
 
+class Migration(migrations.Migration):
     dependencies = [
         ('stein', '0034_merge_20171018_1737'),
     ]
 
     operations = [
-	migrations.RunPython(eliminate_hyphen_in_variety, revert)
-    
-]
+        migrations.RunPython(eliminate_hyphen_in_variety, revert)
+
+    ]
 
 #test
